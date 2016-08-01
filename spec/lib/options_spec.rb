33e314df671703eec5b5eae1696aabb4fb71efbb
Dave Schweisguth <dave@schweisguth.org>
2016-08-01 07:24:05 -0700

    Test (and suppress) stderr in tests of incorrect arguments

--- spec/lib/options_spec.rb
+++ spec/lib/options_spec.rb
@@ -12,29 +12,62 @@ describe ImageSite::Options do
       expect(options.files).to eq(%w(image.jpeg))
     end
 
-    it "aborts if a title was not specified" do
-      stub_const 'ARGV', %w(-o output image.jpeg)
-      expect { options.parse! }.to raise_error SystemExit
-    end
+    context "watching stderr" do
+      class Watcher
+        attr_reader :output
 
-    it "aborts if an output dir was not specified" do
-      stub_const 'ARGV', %w(-t Title image.jpeg)
-      expect { options.parse! }.to raise_error SystemExit
-    end
+        def initialize
+          @output = []
+        end
 
-    it "aborts if one or more files were not specified" do
-      stub_const 'ARGV', %w(-t Title -o output)
-      expect { options.parse! }.to raise_error SystemExit
-    end
+        def write(string)
+          @output << string
+        end
 
-    it "aborts if an unknown option was specified" do
-      stub_const 'ARGV', %w(-x)
-      expect { options.parse! }.to raise_error SystemExit
-    end
+      end
 
-    it "prints help and exits if -h was specified" do
-      stub_const 'ARGV', %w(-h)
-      expect { options.parse! }.to raise_error SystemExit
+      let(:stderr) { Watcher.new }
+
+      around do |example|
+        old_stderr = $stderr
+        $stderr = stderr
+        begin
+          example.run
+        ensure
+          $stderr = old_stderr
+        end
+      end
+
+      it "aborts if a title was not specified" do
+        stub_const 'ARGV', %w(-o output image.jpeg)
+        expect { options.parse! }.to raise_error SystemExit
+        expect(stderr.output.first).to match(/Please specify a title with -t\./)
+      end
+
+      it "aborts if an output dir was not specified" do
+        stub_const 'ARGV', %w(-t Title image.jpeg)
+        expect { options.parse! }.to raise_error SystemExit
+        expect(stderr.output.first).to match(/Please specify an output directory with -o\./)
+      end
+
+      it "aborts if one or more files were not specified" do
+        stub_const 'ARGV', %w(-t Title -o output)
+        expect { options.parse! }.to raise_error SystemExit
+        expect(stderr.output.first).to match(/Please specify one or more image files\./)
+      end
+
+      it "aborts if an unknown option was specified" do
+        stub_const 'ARGV', %w(-x)
+        expect { options.parse! }.to raise_error SystemExit
+        expect(stderr.output.first).to start_with('Usage:')
+      end
+
+      it "prints help and exits if -h was specified" do
+        stub_const 'ARGV', %w(-h)
+        expect { options.parse! }.to raise_error SystemExit
+        expect(stderr.output.first).to start_with('Usage:')
+      end
+
     end
 
   end
